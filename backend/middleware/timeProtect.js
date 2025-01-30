f0dbfe22ce39bc6e24a000935fea3d190d4aee89
Anthony Kercher <anthony@fprintf.com>
2025-01-29 20:14:47 -0600

    fix time middleware

--- backend/middleware/timeProtect.js
+++ backend/middleware/timeProtect.js
@@ -1,9 +1,9 @@
 const time = require("../config/time.js");
 
 const protect = async (req, res, next) => {
-  console.log(process.env)
-  const cur = new Date(await time.time()).getHours()
-  console.log(new Date().getHours())
+  const timeNTP = new Date(await time.time());
+  const cur = timeNTP.getHours()
+  console.log(timeNTP.toString())
   const curfewStart = parseInt(process.env.ALLOW_START);
   const curfewEnd = parseInt(process.env.ALLOW_END);
   const bypass = process.env.BYPASS_CURFEW == "true" || process.env.BYPASS_CURFEW == true
